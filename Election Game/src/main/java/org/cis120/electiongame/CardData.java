36652d4bdf8e26e8c041626b17b63943399fb48f
kevinmani <kevinmani@kevins-air.fios-router.home>
2023-01-20 01:39:31 -0500

    1/20 1:39 AM Kevin Mac

--- Election Game/src/main/java/org/cis120/electiongame/CardData.java
+++ Election Game/src/main/java/org/cis120/electiongame/CardData.java
@@ -2152,22 +2152,18 @@ public class CardData {
             "Kyrsten Sinema", "West", 4, 1, 2, 3, "Progressive", "Anti-Establishment",
             new String[]{"Present Era"}
     );
-    
     static President chuckGrassley = new President(
             "Chuck Grassley", "Midwest", 9, 2, 2, 2, "Conservative", "Subsidies",
             new String[]{"Present Era"}
     );
-    
     static President eliseStefanik = new President(
             "Elise Stefanik", "Northeast", 3, 2, 3, 2, "Conservative", "Establishment",
             new String[]{"Present Era"}
     );
-    
     static President jaredGolden = new President(
             "Jared Golden", "Northeast", 2, 2, 3, 1, "Populist", "Personal Freedom",
             new String[]{"Present Era"}
     );
-    
     static President anthonyDevolder = new President(
             "Anthony Devolder", "Northeast", 10, 10, 10, 10, "Conservative", "Nationalism",
             new String[]{"Meme Card"}
